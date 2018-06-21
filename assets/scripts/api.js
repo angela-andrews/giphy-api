48165cfe609843ba28dcc0729075ea686175a112
Angela Andrews <aandrews17@yahoo.com>
2018-06-21 14:44:14 -0400

    Gifs on page, data attrs on images, state won't change on click

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -2,6 +2,7 @@
 var topics = ["Janet Jackson","Low Down Dirty Shame","Beyonce", "Blackish", "Game Of Thrones","Curb Your Enthusiam","Sons of Anarchy","ANTM","Eye roll", "Black Panther"];
 
 
+
 //make buttions from topics array
 function makeButtons (){
     var  btnHere = $("#gifBtn") ;
@@ -15,12 +16,10 @@ function makeButtons (){
         btnHere.append(arrayBtns );
         
     };
-};
-
+};//end makeButtons()
+makeButtons();
   
-//   $("#findGifs").on("click", function(event) {
-//from button to submit
-//grab the text from the form & push it onto the topics array, then re-render the buttons
+    //get new topics from form
     $("form").submit(function(){
     event.preventDefault();
     
@@ -29,46 +28,77 @@ function makeButtons (){
     //console.log(moreTopics);
     makeButtons();
     //testApi();
-  });
+    }); //end push to topics function
 
-function getMoreTopics(){
+    //listener for the dynamically created buttons
+
+  $(document).on("click",".displayGifsBtn", function(){
     var getGiphys = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + getGiphys + "&api_key=dc6zaTOxFJmzC&limit=10&rating=g&";
-
-    //console.log(queryURL);
+    console.log(getGiphys);
+    console.log(queryURL);
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response){
         console.log(response);
 
-        //get static, non-animated gifs on page
         
-        //rating under each image
+            var returned = response.data;
+            for (var j= 0; j < topics.length ; j++){
+                //create div for gifs
+                var imgDiv = $("<div>");
+                //create p tags for ratings text
+                var ratingP = $("<p>").text("Rating: " + returned[j].rating);
+                //create the img placeholder
+                var gifImg = $("<img>");
+                gifImg.attr("src", returned[j].images.fixed_height_still.url);
+                //add attr of still image
+                gifImg.attr("data-state", "still");
+                gifImg.attr("data-still", staticImgsURL );
+                gifImg.attr("data-animated", animatedImgsURL );
+                gifImg.addClass("animate");
+                //add atr of data-state
+                var staticImgsURL= (returned[j].images.fixed_height_still.url);
+                //put static img url in var
+                var animatedImgsURL=(returned[j].images.fixed_height.url);
+                //append rating to imgdiv
+                imgDiv.append(gifImg);
+                //put animated img url in var
+                imgDiv.append(ratingP);
+                //append gif to imgdiv
+                $("#displayGifs").prepend(imgDiv);
+                //prepend imgdivs to the page
+                console.log("Animated URL " +animatedImgsURL);
+                console.log("Static URL " +staticImgsURL);
 
-        //when static  is clicked, gifs animates & it toggles with each image click
+            }; //end for Loop 
+        
+        
+    }) //end repsonse   
+
+}); //findGifs on click function
+$("body").on("click",".animate", function(){
+    var state = $(this).attr("data-state");
+    if (state ==="still"){
+        alert("my state is " + state);
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("date-state", "animate");
+    } else{
+        alert("my state is " + state);
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
 
 
-    });
-};
 
 
-// function testApi(){
-//     var newArtist = 'drake';       
-//     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + newArtist + "&api_key=dc6zaTOxFJmzC&limit=10";       
-    
-//     $.ajax({
-//         url: queryURL,          
-//         method: "GET"      
-//      }).done(function(response){ 
-//           console.log(response);
-//        });
-    
-//     };
 
 
-//listener for the dynamically created buttons
-$(document).on("click", ".displayGifsBtn", 
-getMoreTopics);
+
+
+
+
 
 makeButtons();
